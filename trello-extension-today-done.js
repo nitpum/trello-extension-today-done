a6947a3c352372230838896c4c85b52f0391d757
nitpum <nitpum@outlook.com>
2019-07-14 23:09:26 +0700

    Use express instead

--- trello-extension-today-done.js
+++ trello-extension-today-done.js
@@ -2,7 +2,12 @@
 * @param context {WebtaskContext}
 */
 const axios = require('axios');
+const express = require('express');
+const app = express();
+const Webtask    = require('webtask-tools');
 
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
-};
+app.get('/', function(req, res) {
+  
+});
+
+module.exports = Webtask.fromExpress(app);
