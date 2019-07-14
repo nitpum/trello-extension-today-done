067f02b0451f7876a1b9a2f4c360c8a9edb769f3
nitpum <nitpum@outlook.com>
2019-07-14 23:23:36 +0700

    Add getBoard function

--- trello-extension-today-done.js
+++ trello-extension-today-done.js
@@ -6,8 +6,29 @@ const express = require('express');
 const app = express();
 const Webtask    = require('webtask-tools');
 
-app.get('/', function(req, res) {
-  
-});
+axios.defaults.baseURL = 'https://api.trello.com/1/';
+
+const getBoardLists = function (boardId) {
+  return axios
+          .get(`boards/${id}/lists?token=${token}&key=${key}`)
+          .then(function (response) {
+            return response.data
+          })
+          .catch(function () {
+            return null
+          })
+}
+
+const moveAllListCardsToList = function (req, res) {
+  const token = req.webtaskContext.secrets.token;
+  const key = req.webtaskContext.secrets.key;
+  const id = req.params.id;
+  getBoardLists()
+    .then(function (data) {
+      res.sendJson(data);
+    })
+}
+
+app.get('/cards/:id/', moveAllListCardsToList);
 
 module.exports = Webtask.fromExpress(app);
