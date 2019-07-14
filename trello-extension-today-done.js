020de57563594bef56ad40325b6dccab9754180a
nitpum <nitpum@outlook.com>
2019-07-14 23:04:43 +0700

    Add axios module

--- trello-extension-today-done.js
+++ trello-extension-today-done.js
@@ -1,6 +1,8 @@
 /**
 * @param context {WebtaskContext}
 */
+const axios = require('axios');
+
 module.exports = function(context, cb) {
   cb(null, { hello: context.query.name || 'Anonymous' });
 };
